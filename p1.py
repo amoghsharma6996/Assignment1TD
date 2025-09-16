d4aeadc9cb76c4a1465cd2e9b1bdd63c6b234978
amoghsharma6996 <amoghsharma6996@gmail.com>
2025-09-16 23:42:40 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,8 @@
+x = int(input("Enter the First Number: "))
+y= int(input("Enter The Second Number: "))
+
+print("Addition: ",x+y)
+print("Subtraction: ",x-y)
+print("Multiplication: ",x*y)
+print("Divison: ",x/y)
+
